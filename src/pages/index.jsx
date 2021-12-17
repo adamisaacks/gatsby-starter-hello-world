1835fa856e96c122d423d7548b39f9b7c32caa6b
AdamIsaacks <adamisaacks@gmail.com>
2021-12-17 17:42:12 -0600

    blurred

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -53,28 +53,28 @@ export default function Home() {
         <ParallaxLayer offset={1.75} speed={2}>
           <div className="absolute h-[8%] w-[8%] left-1/4">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/AboutMe/AppleLogo.svg"
             />
           </div>
 
           <div className="absolute h-[15%] w-[15%] -top-1/4 left-3/4">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/AboutMe/TAMULogo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] -top-1/4 left-[10%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/AboutMe/BPLogo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] top-1/4 left-[70%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/AboutMe/TraneLogo.svg"
             />
           </div>
@@ -113,42 +113,42 @@ export default function Home() {
         <ParallaxLayer offset={3.75} speed={2}>
           <div className="absolute h-[8%] w-[8%] left-1/4">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/GatsbyLogo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] -top-1/4 left-3/4">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/CSS3Logo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] -top-1/2 left-1/2">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/HTML5Logo.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] top-1/4 left-[10%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/JSLogo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] -top-1/4 left-[10%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/NodeLogo.png"
             />
           </div>
 
           <div className="absolute h-[10%] w-[10%] top-1/4 left-[70%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Software/ReactLogo.png"
             />
           </div>
@@ -187,112 +187,112 @@ export default function Home() {
         <ParallaxLayer offset={5.75} speed={2}>
           <div className="absolute h-[7%] w-[7%] left-[6%] top-[6%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/br.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[74%] -top-[6%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/ca.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[24%] -top-[10%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/cn.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[86%] top-[9%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/de.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[63%] -top-[34%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/gr.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[18%] top-[36%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/hu.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[8%] -top-[27%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/id.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[47%] ">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/is.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[30%] top-[22%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/jp.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[81%] top-[40%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/mx.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[30%] -top-[38%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/nl.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[74%] top-[75%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/pa.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[24%] top-[60%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/pt.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[57%] top-[57%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/th.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[47%] top-[40%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/us.png"
             />
           </div>
 
           <div className="absolute h-[7%] w-[7%] left-[14%] top-[80%]">
             <StaticImage
-              placeholder="tracedSVG"
+              placeholder="blurred"
               src="../assets/images/Travel/vn.png"
             />
           </div>
