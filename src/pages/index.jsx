b86e0d3f87e0122e00be82d0e40a7d6d3f8d1ca9
AdamIsaacks <adamisaacks@gmail.com>
2021-12-20 01:57:45 -0600

    blurrrry

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,8 +7,8 @@ import { Disclosure } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 
 const navigation = [
-  { name: "Home", href: "#", current: true, place: 0 },
-  { name: "Engineering", href: "#", current: false, place: 1 },
+  { name: "Home", href: "#", current: false, place: 0 },
+  { name: "Engineer", href: "#", current: false, place: 1 },
   { name: "Software", href: "#", current: false, place: 2.5 },
   { name: "Travel", href: "#", current: false, place: 4 },
   { name: "Contact", href: "#", current: false, place: 6 },
@@ -21,7 +21,7 @@ function classNames(...classes) {
 export default function Home() {
   const goTo = useRef(null)
   return (
-    <div className="text-gray-200">
+    <div className="text-gray-100">
       {/* All this is the Navbar, trust I me I would have put this in a component if I could */}
       <div className="fixed z-20 w-screen">
         <Disclosure
@@ -31,10 +31,10 @@ export default function Home() {
           {({ open }) => (
             <>
               <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-16">
+                <div className="relative flex items-center justify-between h-16 backdrop-blur-md">
                   <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     {/* Mobile menu button*/}
-                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
                         <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -56,8 +56,8 @@ export default function Home() {
                             href={item.href}
                             className={classNames(
                               item.current
-                                ? " font-bold text-white"
-                                : "text-gray-200 hover:font-bold hover:text-white",
+                                ? " font-bold text-gray-100"
+                                : "text-gray-100 hover:font-bold hover:text-white",
                               "px-3 py-2 rounded-md text-sm font-medium"
                             )}
                             aria-current={item.current ? "page" : undefined}
@@ -73,7 +73,7 @@ export default function Home() {
               </div>
               {/* These are the mobile buttons */}
               <Disclosure.Panel className="sm:hidden">
-                <div className="px-2 pt-2 pb-3 space-y-1">
+                <div className="px-2 pt-2 pb-3 space-y-1 backdrop-blur-md shadow-md">
                   {navigation.map(item => (
                     <Disclosure.Button
                       key={item.name}
@@ -81,8 +81,8 @@ export default function Home() {
                       href={item.href}
                       className={classNames(
                         item.current
-                          ? "font-bold text-white"
-                          : "text-gray-200 hover:font-bold hover:text-white",
+                          ? "font-bold text-gray-100"
+                          : "text-gray-100 hover:font-bold hover:text-white",
                         "block px-3 py-2 rounded-md text-base font-medium"
                       )}
                       aria-current={item.current ? "page" : undefined}
